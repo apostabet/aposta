ea833c83ec8ebaf54901ce00302a766d532ce6be
Herbet Junior <herbetjr@gmail.com>
2023-02-07 13:27:51 -0300

    dsds

---
+++ storage/framework/views/2b32bd37fa49e4aa3fcb13966ef8afddc014641d.php
@@ -0,0 +1,157 @@
+<?php $__env->startSection('title', trans('Regras')); ?>
+<?php $__env->startSection('content'); ?>
+
+
+    <section class="about-section">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-12">
+                    <p><strong>1 -&nbsp;Termos e Condições</strong></p>
+                    <p>1.1 - Proibido para menores de 18 (dezoito anos).</p>
+                    <p>1.2 - Cada pessoa poderá ter apenas uma conta. A conta do usuário deverá ser registrada com
+                        dados&nbsp;válidos e deve ser usado somente pelo titular da conta, sendo assim o titular total
+                        responsável pela mesma.</p>
+                    <p>1.3 - O jogador/cambista está ciente que poderá perder valores. A <strong>ImperadorBet</strong> não
+                        assume qualquer responsabilidade por qualquer ação de terceiros.</p>
+                    <p>1.4 - A <strong>ImperadorBet</strong> não assume quaisquer violações de leis locais ou nacionais
+                        perpetrados pelo jogador. É de exclusiva responsabilidade do mesmo responder, em partes fiscais e/ou
+                        competentes, em caso de má conduta/irresponsabilidade qualquer.</p>
+                    <p>1.5 - Independente de alegar conhecimento ou não, as regras aqui citadas prevalecerão, por isso
+                        recomendamos a leitura desse regulamento pelo menos uma vez por mês. Não nos responsabilizamos por
+                        qualquer informação divergente das aqui expostas.</p>
+                    <p>1.6 - Serão considerados nulo/sem premiação toda atividade que julgarmos como tentativa de burlar o
+                        sistema e suas regras.</p>
+                    <p>1.7 - Procedimentos/Documentações fraudulentas e/ou de cunho duvidoso; toda atividade
+                        suspeita/contrária ao permitido por regra será automaticamente cancelada e não validada para
+                        qualquer tipo de premiação/validação.</p>
+                    <p>1.8 - Pagamentos de premiações serão efetuadas apenas aos nominais de titulares de contas
+                        cadastradas.</p>
+                    <p>1.9 -&nbsp;Caso nossos&nbsp;analistas de jogos, encontre no&nbsp;login do Cambista&nbsp;diversas
+                        apostas seguindo um determinado&nbsp;padrão, será entendido que as apostas estão partindo de um
+                        mesmo apostador, logo, a permanência deste cambista é inviável, o seu login será bloqueado
+                        imediatamente e as apostas suspeitas canceladas sem aviso prévio.</p>
+                    <p><strong>2 -&nbsp;Validade e Aceitação</strong></p>
+                    <p>2.1 - O valor <strong>mínimo</strong> por aposta em bilhete, independentemente do número e valores de
+                        jogos, é de R$ <strong>2,00, salvo, </strong>ajuste com cambista.</p>
+                    <p>2.2 - O valor máximo por aposta em bilhete, independentemente do número e valores de jogos, é de R$
+                        1000,00. Todavia, irá depender diretamente do limite estabelecido por cada Supervisor ou Cambista
+                        independente.</p>
+                    <p>2.3 - O valor máximo de premiação, independentemente do número e valores de jogos, é de R$ 5.000,00
+                        por bilhete, salvo, configuração individual de Cambistas independentes ou Supervisores.</p>
+                    <p>2.4 - Todo bilhete só será validado com a cotação mínima de 1,60. Não é possível finalizar um jogo
+                        com a soma dos valores de cotação menor que o mesmo.&nbsp;</p>
+                    <p>2.4.1 - Bilhetes iguais com os mesmo jogos, que a soma deles passem do valor teto estabelecido pelo
+                        supervisor ou cambista independente serão Cancelados e/ou sob pena de banimento do apostador.&nbsp;
+                    </p>
+                    <p>2.5 - O fator multiplicador máximo para cada bilhete, independente dos jogos é de 500 vezes o valor
+                        apostado. (Ex: um bilhete de valor R$10,00 terá como premiação máxima R$5.000,00 independente dos
+                        jogos/cotações incluídos) para bilhetes de até 500 (Quinhentos reais!) acima desse valor a
+                        multiplicação é 10 vezes do valor apostado!</p>
+                    <p>2.6 - Tentativa de burlar o sistema à tirar proveitos em opções distintas de dupla possibilidade
+                        serão anuladas. (Ex: Time1 x Time2. Time1 pagando 5,50, Time2 Pagando 1,50, Empate pagando 4,50. 1X
+                        (casa com empate) paga 1,75. Jogador faz bilhete único no Time1 e outro jogo de bilhete único no
+                        Empate. Em caso de premiação o jogo será inválido e os bilhetes serão anulados; e se perder sabendo
+                        da regra, perdeu.)</p>
+                    <p>2.7 - Jogos não iniciados, transferidos, adiados, remarcados ou interrompidos (por menos de 30
+                        minutos da partida), e que não tenham continuação em até 24 horas do ponta-pé inicial; o jogo será
+                        cancelado e o mesmo sairá do bilhete, mantendo assim os demais resultados. Caso esse jogo seja
+                        único, o bilhete será desconsiderado. Em contrárias&nbsp;situações, mantêm-se&nbsp;o resultado da
+                        partida do momento.</p>
+                    <p>2.8 - Todo jogo (futebol) terá validade apenas de 90 minutos mais acréscimos, não valendo assim
+                        resultados de penalidades, gol de ouro, prorrogações, súmulas, WO ou qualquer outro fator que altere
+                        posteriormente o resultado da partida finalizada por motivos diversos. salvo, escanteios nas
+                        prorrogações que valerão normalmente.&nbsp;</p>
+                    <p>2.9. - Consideraremos apenas jogos iniciados dentro do horário exato. Em caso de partida já iniciada
+                        ou atrasada&nbsp;e/ou, por algum erro de sistema&nbsp;ou por qualquer outro motivo começar fora do
+                        horário (por&nbsp;horário do sistema ou bilhete, tiver alguma divergência)&nbsp;a
+                        <strong>ImperadorBet</strong> julgará a partida inválida em casos de tirar proveito da situação e
+                        cancelará a mesma automaticamente, independente de jogo finalizado ou não.</p>
+                    <p>2.10 - Proibido e não permitido apostas seca (1 palpite no bilhete) dentro do ao vivo dentro da
+                        imperadorbet.</p>
+                    <p>2.X - Em caso da partida que tenha o mandante/visitante invertido ou errado no sistema, o jogo será
+                        anulado, exceto se a partida for em campo neutro e/ou time da mesma cidade, e/ou amistosos.</p>
+                    <p>2.X.2 - Jogos com cotações (odds) diferentes das normais (média de 20% de diferença), jogos com
+                        suspeitas de fraude/manipulação de resultados, bilhetes feitos de contra as regras (seja por
+                        problema de sistema ou não);&nbsp;serão canceladas sem aviso prévio&nbsp;independente do horário dos
+                        jogos, bilhetes já premiados, pendentes ou perdidos.</p>
+                    <p>&nbsp;</p>
+                    <p><strong>Conclusão</strong></p>
+                    <p>Automaticamente o cliente/jogador que fizer o cadastro ou bilhete&nbsp;estará de acordo com todas as
+                        regras pré-estabelecidas, independente de terem lido&nbsp;ou não, visto que no ato do mesmo, serão
+                        orientados a lerem as regras atenciosamente.</p>
+                    <p>A <strong>ImperadorBet</strong>&nbsp;reserva todos os direitos de cancelamento/não pagamento sem
+                        aviso prévio para qualquer motivo que julgar considerado como má fé de clientes/cambistas tais como:
+                        erros de cotação por sistema ou falha manual, horário divergente do iniciado, bilhete emitido de
+                        contra as regras do site ou qualquer tipo de sinistro relativo a atividade, documentação falsa,
+                        suspeita manipulação de resultados, aproveitamento de uma situação inesperada seja do sistema ou
+                        não, enfim; tudo que julgar tentativa de tirar proveito de determinadas situações acima citadas
+                        anteriormente.</p>
+                    <p><strong>&nbsp;</strong></p>
+                    <p><strong>PAGAMENTO DE PRÊMIO</strong></p>
+                    <ol>
+                        <li>O pagamento dos apostadores ganhadores se dará da seguinte forma:</li>
+                        <li>A <strong>ImperadorBet</strong> ( na figura do respectivo supervisor responsável) terá o prazo
+                            de&nbsp;até 72&nbsp;horas úteis, pós acerto com o cambista para pagar qualquer valor ganho pelo
+                            apostador.</li>
+                    </ol>
+                    <p>2.1 Em conta bancária ou outro meio lícito, indicado pelo apostador ou Cambista responsável.</p>
+                    <ol start="3">
+                        <li>A responsabilidade de pagamento ao apostador ganhador é inteiramente do
+                            <strong>Supervisor</strong> <strong>ImperadorBet</strong>, salvo, acordo direto e exclusivo com
+                            o cambista ou gerente.</li>
+                        <li>Não poderá ser requisitado prêmio de bilhete validado por cambista teste, no próprio bilhete
+                            terá a descrição, (bilhete não válido) sob pena de banimento do potêncial parceiro.</li>
+                    </ol>
+                    <p><strong>BANIMENTO DO SUPERVISOR</strong></p>
+                    <p><strong>1. A AS SISTEMAS NÃO É RESPONSÁVEL POR NENHUM PRÊMIO, AS SISTEMAS é uma empresa de sistemas e
+                            apenas aluga logins de supervisores.</strong></p>
+                    <p><strong>2. </strong>Comprovado que o responsável em pagar o prêmio do apostador assim o deixou de
+                        fazer, será imediatamente banido do sistema, perdendo todo seu acesso a plataforma.</p>
+                    <p><strong>3.</strong> <strong>AS SISTEMAS</strong> não é responsável por prêmios, todavia, zela pela
+                        marca da ImperadorBet, então caso você tenha sido lesado(a) por algum supervisor, no ítem 4 iremos
+                        disponibilizar um contato de e-mail pela qual você poderá encaminhar sua denúncia.&nbsp;&nbsp;</p>
+                    <p><strong>4.</strong> E-mail de contato para denúncia: (denunciaimperadorbet@gmail.com, por favor envie
+                        o e-mail juntamente com provas.&nbsp;</p>
+                    <p><strong>REGRAS DO JOGO AO VIVO</strong></p>
+                    <p>1.1 - O Bilhete só será validado no jogo ao vivo com (DOIS) ou mais times.</p>
+                    <p>1.2 - As cotações elas ficam oscilando, o mercado de cotações pode fechar e abrir em qualquer momento
+                        da partida.</p>
+                    <p>1.2.1 - O valor máximo por aposta em bilhete, independentemente do número e valores de jogos, é de R$
+                        300,00.</p>
+                    <p>1.3 - O jogo pode ser retirado a qualquer momento do ao vivo!</p>
+                    <p>1.4 - Aos 85 minutos do segundo tempo, o jogo é retirado do ao vivo.</p>
+                    <p>1.4.2 - O valor máximo de premiação, independentemente do número e valores de jogos, é de R$ 5.000,00
+                        por bilhete.</p>
+                    <p>1.5 - Independente de alegar conhecimento ou não, as regras aqui citadas prevalecerão, por isso
+                        recomendamos a leitura desse regulamento pelo menos uma vez por mês. Não nos responsabilizamos por
+                        qualquer informação divergente das aqui expostas.</p>
+                    <p>1.6 - Serão considerados nulo/sem premiação toda atividade que julgarmos como tentativa de burlar o
+                        sistema e suas regras.</p>
+                    <p>1.7 O Bilhete feito na aposta ao vivo, não poderá ser mais cancelado.</p>
+                    <p>1.8 A Equipe <strong>ImperadorBet</strong>, Tem uma equipe especializada em verificação de todas as
+                        apostas.&nbsp;</p>
+                    <p>&nbsp;</p>
+                    <p><strong><u>JOGO RESPONSÁVEL:<br></u></strong></p>
+                    <div>A&nbsp;<strong>ImperadorBet</strong> está empenhada em promover o&nbsp;<strong>JOGO
+                            RESPONSÁVEL</strong>.&nbsp;</div>
+                    <div>Promovemos o jogo como uma atividade de lazer e acreditamos que o jogo apenas poderá ser desfrutado
+                        desta forma se mantiver o controle e jogar de forma responsável. No entanto, sabemos que para
+                        algumas pessoas, o jogo pode deixar de ser uma atividade de lazer inofensiva e tornar-se um
+                        problema.<br> 1. O jogo deve ser visto como uma forma de entretenimento e não como uma forma de
+                        fazer dinheiro.<br> 2. Aposte com sensatez e nunca persiga perdas.<br> 3. Aposte apenas aquilo que
+                        se pode dar ao luxo de perder.<br> 4. Monitorize o tempo que passa a jogar.<br> 5. Faça pausas do
+                        jogo regularmente. Jogar de forma contínua pode fazer com que perca noção de tempo e
+                        perspectiva.<br> 6. Não jogue sob influência de álcool ou quando se sente aborrecido (a) ou
+                        deprimido (a).<br> Se possuir preocupações com o seu jogo, favor nos contatar para o cancelamento da
+                        conta.&nbsp;</div>
+                    <div>&nbsp;</div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make($theme . 'layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\laragon\www\aposta\resources\views/themes/betting/rules.blade.php ENDPATH**/ ?>
